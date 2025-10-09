466bd69d3db9474f15455b356049d495a0fe4643
Flora Hill <imflo.pink@gmail.com>
2025-10-09 21:09:27 +1100

    whoops

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Zen Browser for multiple platforms";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
@@ -9,34 +11,58 @@
         inherit (nixpkgs.legacyPackages.x86_64-linux) fetchurl fetchFromGitHub fetchgit dockerTools;
       };
   
-  mkPackages = system: 
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      
-      # AppImage package (Linux x86_64 and aarch64)
-      mkAppImage = source: pkgs.appimageTools.wrapType2 {
-        inherit (source) pname version src;
-        extraInstallCommands = ''
-          mv $out/bin/${source.pname} $out/bin/zen-browser
-        '';
+      mkPackages = system: 
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          
+          # AppImage package (Linux x86_64 and aarch64)
+          mkAppImage = source: pkgs.appimageTools.wrapType2 {
+            inherit (source) pname version src;
+            extraInstallCommands = ''
+              mv $out/bin/${source.pname} $out/bin/zen-browser
+            '';
+            meta = {
+              description = "Privacy-focused browser that blocks trackers, ads, and other unwanted content while offering the best browsing experience!";
+              homepage = "https://github.com/zen-browser/desktop";
+              platforms = [ "aarch64-linux" "x86_64-linux" ];
+              sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
+              license = pkgs.lib.licenses.mpl20;
+            };
+          };
+          
+          # DMG package (macOS) - using undmg
+          mkDMG = source: pkgs.stdenv.mkDerivation {
+            inherit (source) pname version src;
+            nativeBuildInputs = [ pkgs.undmg ];
+            sourceRoot = "Zen Browser.app";
+            installPhase = ''
+              mkdir -p $out/Applications
+              cp -r "Zen Browser.app" $out/Applications/
+              mkdir -p "$out/Applications/Zen Browser.app/Contents/Resources/distribution"
+            '';
+            meta = {
+              description = "Privacy-focused browser that blocks trackers, ads, and other unwanted content while offering the best browsing experience!";
+              homepage = "https://github.com/zen-browser/desktop";
+              platforms = [ "aarch64-darwin" "x86_64-darwin" ];
+              sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
+              license = pkgs.lib.licenses.mpl20;
+            };
+          };
+          
+        in {
+          zen-browser = 
+            if system == "x86_64-linux" then mkAppImage sources.x64
+            else if system == "aarch64-linux" then mkAppImage sources.aarch64  
+            else if system == "aarch64-darwin" || system == "x86_64-darwin" then mkDMG sources.darwin
+            else throw "Unsupported system: ${system}";
+        };
+    in
+    {
+      packages = {
+        x86_64-linux = mkPackages "x86_64-linux" // { default = (mkPackages "x86_64-linux").zen-browser; };
+        aarch64-linux = mkPackages "aarch64-linux" // { default = (mkPackages "aarch64-linux").zen-browser; };
+        aarch64-darwin = mkPackages "aarch64-darwin" // { default = (mkPackages "aarch64-darwin").zen-browser; };
+        x86_64-darwin = mkPackages "x86_64-darwin" // { default = (mkPackages "x86_64-darwin").zen-browser; };
       };
-      
-      # DMG package (macOS)
-      mkDMG = source: pkgs.stdenv.mkDerivation {
-        inherit (source) pname version src;
-        nativeBuildInputs = [ pkgs.undmg ];
-        sourceRoot = "Zen Browser.app";
-        installPhase = ''
-          mkdir -p $out/Applications
-          cp -r "Zen Browser.app" $out/Applications/
-        '';
-      };
-
-  meta = {
-    description = "Privacy-focused browser that blocks trackers; ads; and other unwanted content while offering the best browsing experience!";
-    homepage = "https://github.com/zen-browser/desktop";
-    platforms = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
-    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
-    license = lib.licenses.mpl20;
-  };
+    };
 }
