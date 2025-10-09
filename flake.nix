f50211497e676fa4cedff3d05718053044ab0221
Flora Hill <imflo.pink@gmail.com>
2025-10-09 21:38:58 +1100

    fix args + precommit

--- flake.nix
+++ flake.nix
@@ -5,22 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       sources = import ./_sources/generated.nix {
         inherit (nixpkgs.legacyPackages.x86_64-linux) fetchurl fetchFromGitHub fetchgit dockerTools;
       };
-  
-      mkPackages = system: 
+
+      mkPackages = system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          
+
           # AppImage package (Linux x86_64 and aarch64)
           mkAppImage = source: pkgs.appimageTools.wrapType2 {
             inherit (source) pname version src;
             extraInstallCommands = ''
               mv $out/bin/${source.pname} $out/bin/zen-browser
-              
+
               # Install desktop entry
               install -Dm644 ${pkgs.writeText "zen-browser.desktop" ''
                 [Desktop Entry]
@@ -37,9 +37,8 @@
                 StartupWMClass=zen-browser
                 StartupNotify=true
               ''} $out/share/applications/zen-browser.desktop
-              
-              # Install icon using appimageTools' icon extraction
-              ${pkgs.appimageTools.extractType2 { inherit (source) src; }}/bin/extract-icon zen-browser $out/share/icons/hicolor/scalable/apps/zen-browser.png || \
+
+              # Install fallback icon
               install -Dm644 ${pkgs.writeText "zen-browser.svg" ''
                 <?xml version="1.0" encoding="UTF-8"?>
                 <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
@@ -56,7 +55,7 @@
               license = pkgs.lib.licenses.mpl20;
             };
           };
-          
+
           # DMG package (macOS) - using undmg
           mkDMG = source: pkgs.stdenv.mkDerivation {
             inherit (source) pname version src;
@@ -75,11 +74,11 @@
               license = pkgs.lib.licenses.mpl20;
             };
           };
-          
+
         in {
-          zen-browser = 
+          zen-browser =
             if system == "x86_64-linux" then mkAppImage sources.x64
-            else if system == "aarch64-linux" then mkAppImage sources.aarch64  
+            else if system == "aarch64-linux" then mkAppImage sources.aarch64
             else if system == "aarch64-darwin" || system == "x86_64-darwin" then mkDMG sources.darwin
             else throw "Unsupported system: ${system}";
         };
